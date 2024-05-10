54d41b581abd8cb1ffd09b24d600bf015f6cd20a
unwado <mezentsev.igor@gmail.com>
2024-05-10 11:14:33 +0300

    Fixed with_calcs functions

--- HPC_bot/models/manager.py
+++ HPC_bot/models/manager.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sqlalchemy import func, or_, select
+from sqlalchemy.orm import contains_eager, subqueryload
 from typing import Sequence
 
 from . import User, TelegramUser, Person, Calculation, Organization
@@ -11,21 +12,33 @@ async def get_all_with_calcs(
         since: datetime = None,
         remove_blocked: bool = False) -> Sequence[TelegramUser]:
 
-    subquery = select(Calculation.user_id).group_by(Calculation.user_id)
+    query = select(TelegramUser, func.count()).options(
+        subqueryload(TelegramUser.user).joinedload(User.person).joinedload(
+            Person.organization)).join(User).outerjoin(Calculation)
 
     if since is not None:
-        subquery = subquery.where(Calculation.start_datetime >= since)
-
-    query = select(TelegramUser).where(TelegramUser.user_id.in_(subquery))
+        query = query.where(
+            or_(Calculation.start_datetime >= since,
+                Calculation.start_datetime == None))
 
     if remove_blocked:
-        query = query.join(User).where(~User.blocked)
+        query = query.where(~User.blocked)
+
+    query = query.group_by(TelegramUser.tg_id)
 
     async with sessionmaker() as session:
         async with session.begin():
             result = await session.execute(query)
 
-            return result.scalars().all()
+            data = result.all()
+
+    users = []
+    for tg_user, count in data:
+        setattr(tg_user, 'num_calc', count)
+
+        users.append(tg_user)
+
+    return users
 
 
 async def search_users(last_name: str = None,
@@ -78,21 +91,33 @@ async def get_tg_user_with_calcs(tg_id: int = None,
     if tg_id is None and user_id is None:
         return None
 
-    subquery = select(Calculation.user_id).group_by(Calculation.user_id)
+    query = select(TelegramUser, func.count()).options(
+        subqueryload(TelegramUser.user).joinedload(User.person).joinedload(
+            Person.organization)).join(User).outerjoin(Calculation)
 
     if since is not None:
-        subquery = subquery.where(Calculation.start_datetime >= since)
-
-    query = select(TelegramUser).where(TelegramUser.user_id.in_(subquery))
+        query = query.where(
+            or_(Calculation.start_datetime >= since,
+                Calculation.start_datetime == None))
 
     if tg_id is not None:
         query = query.where(TelegramUser.tg_id == tg_id)
 
     if user_id is not None:
-        query = query.join(User).where(User.id == user_id)
+        query = query.where(User.id == user_id)
+
+    query = query.group_by(TelegramUser.tg_id)
 
     async with sessionmaker() as session:
         async with session.begin():
             result = await session.execute(query)
 
-            return result.scalar_one_or_none()
+            data = result.first()
+
+    if data is None:
+        return None
+
+    tg_user, count = data
+    setattr(tg_user, 'num_calc', count)
+
+    return tg_user
